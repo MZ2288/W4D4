a27d1e4f366a68b583eba6b126ca3a03a4ecfe89
Marek Zwick <utahmarek@gmail.com>
2015-11-19 17:51:53 -0800

    whatever

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,51 @@
+class BandsController < ApplicationController
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def create
+    @band = Band.new(band_params)
+
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @band = Band.find_by(:id)
+    render :edit
+  end
+
+  def show
+    @band = Band.find_by(:id)
+    render :show
+  end
+
+  def update
+    @band = Band.find_by(:id)
+    if @band.update(band_params)
+      redirect_to band_url(@band)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @band = Band.find_by(:id)
+    @band.destroy
+    redirect_to bands_url
+  end
+
+  private
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
